61223d8846d3d3a27e9a5e404af8c33db7a29e56
MaxJordan <u1357447@unimail.hud.ac.uk>
2016-10-28 11:28:50 +0100

    change password of the database in the file

--- guestbook.php
+++ guestbook.php
@@ -8,7 +8,7 @@
 $link = mysqli_connect(
     "127.0.0.1", // host
     "root",      // username
-    "root",      // password
+    "password",      // password
     "guestbook"       // schema name
 );
 
